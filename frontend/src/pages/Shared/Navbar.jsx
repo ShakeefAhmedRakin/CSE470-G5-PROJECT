4b0ac320a5bfee5db1134ab0ff7f18057e8d7d18
Shoeb Hamim <hamim99bd@gmail.com>
2024-02-09 23:02:05 +0600

    navbar design initially done

--- frontend/src/pages/Shared/Navbar.jsx
+++ frontend/src/pages/Shared/Navbar.jsx
@@ -1,5 +1,26 @@
 const Navbar = () => {
-  return <></>;
+  return <>
+  <div className="h-28 px-8  shadow-box-shadow m-8 flex items-center justify-between">
+    <div className="flex items-center ">
+      <div className="h-16 mr-6">
+        <img className="h-full" src="logo.png" alt="" />
+      </div>
+      <div>
+        <p className="text-3xl font-bold text-theme-color">BLue Line</p>
+      </div>
+      
+    </div>
+    <div>
+      <button className="mx-12 text-lg font-bold">Home</button>
+      <button className="mx-12 text-lg" >About</button>
+      <button className="mx-12 text-lg" >Support</button>
+      <button className="text-lg bg-theme-color text-white py-6 px-8">SIGN UP</button>
+
+    </div>
+
+  </div>
+  
+  </>;
 };
 
 export default Navbar;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'theme-color': '#4d62d3',
+      },
+      boxShadow:{
+        'box-shadow': "0px 4px 18.799999237060547px 0px #00000040"
+
+      }
+    },
   },
   plugins: [],
 };
