5dea63ae4f057d0b993d45c02eff6d89a793c147
Shakeef Ahmed <shakeef.rakin321@gmail.com>
2024-02-09 21:46:23 +0600

    Installed Tailwind and setup react router routes and basic folder structure.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home/Home";
+import Root from "./pages/Root";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <h1>Login</h1>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ frontend/src/pages/Root.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./Shared/Navbar";
+import Footer from "./Shared/Footer";
+
+const Root = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default Root;
