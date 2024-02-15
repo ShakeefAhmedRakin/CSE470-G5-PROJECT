e7e9adab5a66f1078bbaa55dc852c4e8c8d4980d
IffatMim <iffat.ara.nazmin@g.bracu.ac.bd>
2024-02-16 05:37:06 +0600

    FAQ added

--- frontend/src/pages/Support/Support.jsx
+++ frontend/src/pages/Support/Support.jsx
@@ -4,6 +4,7 @@ const Support = () => {
       <div className="min-h-screen mx-24 ">
         <div className="mb-6" data-aos="fade-up" data-aos-delay="50">
           {/* CODE HERE */}
+          <h1>Check</h1>
         </div>
       </div>
     </>
