7e0f183eaff9c97575ba5691698d9d6bde53e9c9
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2024-03-04 00:38:50 +0600

    Added backend.

---
+++ backend/index.js
@@ -0,0 +1,58 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iixzvov.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const busesCollection = client.db("BlueLine").collection("BusesCollection");
+
+    // GET API FOR ALL BUSES
+    app.get("/all-buses", async (req, res) => {
+      const result = await busesCollection.find().toArray();
+      res.send(result);
+    });
+
+    // GET API FOR ALL BUSES
+    app.get("/bus/get/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await busesCollection.findOne(query);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(port, () => {
+  console.log("Server listening on port", port);
+});
